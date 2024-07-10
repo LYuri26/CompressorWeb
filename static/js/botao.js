8c57251339bcaf28c52f8413bc2161e19c9c56dd
Lenon Yuri <lenonyurisilva26@gmail.com>
2024-07-10 18:17:28 -0300

    10/07

--- static/js/botao.js
+++ static/js/botao.js
@@ -4,7 +4,7 @@ toggleBtn.addEventListener('click', function() {
     if (toggleBtn.textContent === 'Ligar') {
         toggleBtn.textContent = 'Desligar';
         toggleBtn.classList.remove('btn-primary');
-        toggleBtn.classList.add('btn-danger'); // Altere para a classe de botão vermelho que preferir
+        toggleBtn.classList.add('btn-danger');
     } else {
         toggleBtn.textContent = 'Ligar';
         toggleBtn.classList.remove('btn-danger');
