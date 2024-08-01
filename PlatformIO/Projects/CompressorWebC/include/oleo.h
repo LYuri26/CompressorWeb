9cf0eac7b1a34d5c994f66ac47e72d951da9a601
Lenon <lenonyurisilva26@gmail.com>
2024-08-01 00:15:42 -0300

    01/08

--- PlatformIO/Projects/CompressorWebC/include/oleo.h
+++
@@ -1,9 +0,0 @@
-#ifndef OLEO_H
-#define OLEO_H
-
-#include <WebServer.h>
-
-// Declaração da função para configurar a página de nível de óleo
-void setupOleoPage(WebServer& server);
-
-#endif // OLEO_H
--- PlatformIO/Projects/CompressorWebC/include/umidade.h
+++
@@ -1,9 +0,0 @@
-#ifndef UMIDADE_H
-#define UMIDADE_H
-
-#include <WebServer.h>
-
-// Declaração da função para configurar a página de umidade
-void setupUmidadePage(WebServer& server);
-
-#endif // UMIDADE_H
