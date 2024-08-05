2cb705a031b58572c20a70dabd6b6340debf14b2
Lenon Yuri <116133424+LYuri26@users.noreply.github.com>
2024-08-05 15:17:27 -0300

    Delete PlatformIO/Projects/CompressorWebC directory

--- PlatformIO/Projects/CompressorWebC/.pio/build/esp32dev/lib9b5/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib9b5/WebServer/detail/mimetable.cpp.o: \
- C:/Users/Instrutor/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/Instrutor/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/Instrutor/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
